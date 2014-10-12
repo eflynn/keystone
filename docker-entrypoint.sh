6538a656d3cff02c4a3c36481d74d9a332d6ee95
Evan Flynn <evan.flynn@gmail.com>
2014-10-12 16:46:03 -0400

    Add error checking; use Kubernetes service

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,13 +5,20 @@ if [ -z "$KEYSTONE_DB_PASSWORD" ]; then
     exit 1
 fi
 
+MYSQL_SERVICE_HOST=${MYSQL_SERVICE_HOST:-$SERVICE_HOST}
+
+if [ -z "$MYSQL_SERVICE_HOST" ]; then
+    echo >&2 'error: Keystone requires access to a MySQL database'
+    exit 1
+fi
+
 KEYSTONE_DB_USER=${KEYSTONE_DB_USER:-keystone}
 KEYSTONE_DB_NAME=${KEYSTONE_DB_NAME:-keystone}
 
 sed -e "s/%DB_USER%/$KEYSTONE_DB_USER/g" \
     -e "s/%DB_PASSWORD%/$KEYSTONE_DB_PASSWORD/g" \
     -e "s/%DB_NAME%/$KEYSTONE_DB_NAME/g" \
-    -e "s/%DB_HOST%/$MYSQL_PORT_3306_TCP_ADDR/g" \
+    -e "s/%DB_HOST%/$MYSQL_SERVICE_HOST/g" \
     -i /etc/keystone/keystone.conf
 
 exec "$@"
